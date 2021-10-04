d5759ca813936a479ca49eefde3df188a56adf9f
ACER <aditi1chalisay@gmail.com>
2021-10-04 17:41:44 +0545

    notepad

--- testing1.py
+++
@@ -1,4 +0,0 @@
-import pytest
-Ent1 = 'Admin'
-Ent2 = '123'
-@pytest.fixturedef fun():x = 'Linus'    y = 'hello'    return [x, y]def test_1(fun):a = 'hello'    b = 'jello'    assert fun[0] == aassert fun[1] == bdef test_2(fun):a= 'Linus'    b= 'hello'    assert fun[0] == a and fun[1] == bdef test_3(fun):a= 'Linu'    b= 'hell'    assert fun[0] == a and fun[1] == b@pytest.mark.parametrize("Name,Pass,Val1,Val2",[('Admin','123','Admin','123'),
